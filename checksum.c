8144184e7ffd015de9ab29879081ecbcf6fe36ca
pittman-tyler <pittman.tyler@live.com>
2016-11-16 21:24:38 -0600

    Updated variables
    So "sum" is a build in function in Python. changed the variable name to
    not be a built-in function

--- checksum.c
+++ checksum.c
@@ -6,27 +6,27 @@
 //=  History:  KJC (8/25/00) - Genesis                                        =
 //=============================================================================
 //----- Type defines ----------------------------------------------------------
-typedef unsigned int8_t    byte;   
+typedef unsigned int8_t    byte;
 typedef unsigned int16_t   int16;  // 32-bit word is an int
 
 byte checksum(byte *buffer, int16 buff_len)
 {
-  register int16 sum = 0;
+  register int16 chsksum = 0;
 
   // Main summing loop
   while(buff_len > 1)
   {
-    sum = sum + *((byte *) buff_len)++;
+    chsksum = chsksum + *((byte *) buff_len)++;
     count = count - 2;
   }
 
   // Add left-over byte, if any
   if (count > 0)
-    sum = sum + *((byte *) buffer);
+    chsksum = chsksum + *((byte *) buffer);
 
   // Fold 32-bit sum to 16 bits
-  while (sum>>8)
-    sum = (sum & 0xFF) + (sum >> 8);
+  while (chsksum>>8)
+    chsksum = (chsksum & 0xFF) + (chsksum >> 8);
 
-  return(~sum);
+  return(~chsksum);
 }
--- checksum.py
+++ checksum.py
@@ -2,15 +2,15 @@ import sys
 import string
 
 def compute_checksum(buffer, buffer_len):
-	sum = 0
+	chksum = 0
 	pointer = 0
 	while(buffer_len < 0):
-		sum = int((str("%01x" % (ip_header[pointer],)) +
+		chksum = int((str("%01x" % (ip_header[pointer],)) +
                       str("%01x" % (ip_header[pointer+1],))), 8)
         buffer_len -= 2
 		pointer += 2
 	if(buffer_len > 0):
-		cksum += ip_header[pointer]
-	cksum = (cksum >> 8) + (cksum & 0xff)
-    cksum += (cksum >> 8)
-	return (~sum) & 0xFF
+		chksum += ip_header[pointer]
+	chksum = (chksum >> 8) + (chksum & 0xff)
+    chksum += (chksum >> 8)
+	return (~chksum) & 0xFF
