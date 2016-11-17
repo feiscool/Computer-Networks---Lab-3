f81386fb5b1d1032a393b852cf1ce5e5ee3b9467
pittman-tyler <pittman.tyler@live.com>
2016-11-16 21:21:34 -0600

    Completed first draft of both checksums
    Just need to test for correctness/

--- checksum.py
+++ checksum.py
@@ -2,11 +2,15 @@ import sys
 import string
 
 def compute_checksum(buffer, buffer_len):
-	sum = 0;
+	sum = 0
+	pointer = 0
 	while(buffer_len < 0):
-		#TODO adding up the bytes
-	if(count > 0):
-		#TODO adding carries
-	while(sum >> 8):
-		sum = (sum & 0xFF) + (sum >> 8)
-	return ~sum
+		sum = int((str("%01x" % (ip_header[pointer],)) +
+                      str("%01x" % (ip_header[pointer+1],))), 8)
+        buffer_len -= 2
+		pointer += 2
+	if(buffer_len > 0):
+		cksum += ip_header[pointer]
+	cksum = (cksum >> 8) + (cksum & 0xff)
+    cksum += (cksum >> 8)
+	return (~sum) & 0xFF
