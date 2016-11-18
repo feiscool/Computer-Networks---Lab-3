a8132fbb283bd60a3207c358dc6cc4b36ebffe6d
pittman-tyler <pittman.tyler@live.com>
2016-11-17 22:02:03 -0600

    Finished Checksum
    Checksum in Python works. C is a WIP. ALL I WANT IS A FOREACH LOOP IN C

--- checksum.py
+++ checksum.py
@@ -1,16 +1,8 @@
-import sys
-import string
-
-def compute_checksum(buffer, buffer_len):
+def compute_checksum(buf):
 	chksum = 0
 	pointer = 0
-	while(buffer_len < 0):
-		chksum = int((str("%01x" % (ip_header[pointer],)) +
-                      str("%01x" % (ip_header[pointer+1],))), 8)
-        buffer_len -= 2
-		pointer += 2
-	if(buffer_len > 0):
-		chksum += ip_header[pointer]
-	chksum = (chksum >> 8) + (chksum & 0xff)
-    chksum += (chksum >> 8)
-	return (~chksum) & 0xFF
+	for element in buf:
+            chksum += int(str("%02x" % element), 16)
+	    chksum = (chksum >> 8) + (chksum & 0xff)
+	chksum += (chksum >> 8)
+	return (~chksum) & 0xff
